e9d5849856b77c22aef99640e9b753c73b3eb49d
Callan Gilding <me@callangilding.com>
2017-12-15 16:50:42 +1100

    Fix Status.py

--- status.py
+++ status.py
@@ -4,4 +4,4 @@ import simplejson as json
 url = urlopen('https://esi.tech.ccp.is/dev/status/?datasource=tranquility').read()
 url = json.loads(url)
 
-print url.get('players') >> /dev/tty1 # Send to HDMI out
+print 'Players Online: ', url['players']
